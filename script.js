3d59c4d4e70d2f4c42900dc6e0df275dbd992b0f
gwendolinecapron <gwendy.capron@gmail.com>
2026-09-22 12:18:09 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener('click', function (event) {
+
+        const target = document.querySelector(
+            this.getAttribute('href')
+        );
+
+        if (!target) return;
+
+        event.preventDefault();
+
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+
+    });
+
+});
